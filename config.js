23d5774b556db3d3f1b537bf9a2ba168fd158240
zhongjie <zhongjie@howzhi.com>
2017-09-15 10:34:42 +0800

    更新版本说明

--- config.js
+++ config.js
@@ -1,44 +1,5 @@
-// /**
-//  * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
-//  * For licensing, see LICENSE.md or http://ckeditor.com/license
-//  */
-
-// CKEDITOR.editorConfig = function( config ) {
-// 	// Define changes to default configuration here.
-// 	// For complete reference see:
-// 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-// 	// The toolbar groups arrangement, optimized for two toolbar rows.
-// 	config.toolbarGroups = [
-// 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-// 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-// 		{ name: 'links' },
-// 		{ name: 'insert' },
-// 		{ name: 'forms' },
-// 		{ name: 'tools' },
-// 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-// 		{ name: 'others' },
-// 		'/',
-// 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-// 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-// 		{ name: 'styles' },
-// 		{ name: 'colors' },
-// 		{ name: 'about' }
-// 	];
-
-// 	// Remove some buttons provided by the standard plugins, which are
-// 	// not needed in the Standard(s) toolbar.
-// 	config.removeButtons = 'Underline,Subscript,Superscript';
-
-// 	// Set the most common block elements.
-// 	config.format_tags = 'p;h1;h2;h3;pre';
-
-// 	// Simplify the dialog windows.
-// 	config.removeDialogTabs = 'image:advanced;link:advanced';
-// };
-
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -122,6 +83,4 @@ CKEDITOR.editorConfig = function( config ) {
     config.extraPlugins = 'questionblank,smiley,table,font,kityformula,codesnippet,uploadpictures,shortUrl';
     // config.stylesSet = 'my_styles';
     config.codeSnippet_theme = 'zenburn';
-
-    
 };
