71911c9a36e0c1ac0602ee14db93ae63fc1aeca0
LianJion <1995lian1002@gmail.com>
2019-02-24 09:53:38 +0800

    添加上传图片泰文js翻译

---
+++ plugins/uploadpictures/lang/th.js
@@ -0,0 +1,11 @@
+CKEDITOR.plugins.setLang( 'uploadpictures', 'zh-cn', {
+	title: 'การอัปโหลดภาพเป็นกลุ่ม',
+	toolbar: 'การอัปโหลดภาพเป็นกลุ่ม',
+    file_type_tip: 'ประเภทไฟล์รองรับเท่านั้น:',
+	single_file_max_size_tip: 'ขนาดไฟล์เดียวต้องไม่เกิน:',
+	waiting_upload_text: 'รอการอัปโหลด',
+	delete_text: 'เอาออก',
+	uploaded_text: 'อัปโหลด',
+	waiting_finish_tip: 'โปรดรอให้การอัปโหลดไฟล์เสร็จสมบูรณ์ ...',
+	confirm_clear_tip: 'ยังมีไฟล์ในรายการปัจจุบันที่ยังไม่ได้อัพโหลดยืนยันว่ารายการจะถูกล้างดำเนินการต่อหรือไม่?'
+} );
